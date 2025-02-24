e61c4eb71eafe97fc8af89f430f30a367b014cbc
liqinbu <997828621@qq.com>
2025-02-24 20:26:44 +0800

    update strategy

--- test_Strategy.py
+++ test_Strategy.py
@@ -7,58 +7,9 @@ import pandas as pd
 # import talib
 import os
 from tqdm import tqdm
+import sqlite3
+import os
 
-class TestMultiStrategy(unittest.TestCase):
-
-    def setUp(self):
-        self.cerebro = bt.Cerebro()
-        # self.data = bt.feeds.YahooFinanceData(dataname='AAPL', fromdate=datetime.datetime(2020, 1, 1), todate=datetime.datetime(2021, 1, 1))
-        # read csv file csv file is in the format of 'date,股票代码,open,close,high,low,volume,成交额,振幅,涨跌幅,涨跌额,换手率'
-        self.data = bt.feeds.GenericCSVData(dataname='./data/000001.csv', dtformat=('%Y-%m-%d'), datetime=0, open=2, high=4, low=5, close=3, volume=6, openinterest=-1)
-        # 可视化 data 数据，确认数据加载是否正确
-        print('Data Loaded: %d' % len(self.data))
-        for line in self.data:
-            print(line.datetime.date(), line.open, line.high, line.low, line.close, line.volume)
-        
-        self.cerebro.adddata(self.data)
-        self.cerebro.addstrategy(MultiStrategy)
-    '''
-    def test_strategy_initialization(self):
-        self.cerebro.run()
-        strategy = self.cerebro.strategies[0]
-        self.assertIsNotNone(strategy.bbi)
-        self.assertIsNotNone(strategy.macd)
-        self.assertIsNotNone(strategy.rsi)
-        self.assertIsNotNone(strategy.resistance)
-        self.assertIsNotNone(strategy.ma60)
-        self.assertIsNotNone(strategy.vol_ma5)
-        self.assertIsNotNone(strategy.drawdown)
-    
-    def test_generate_bbi_signal(self):
-        self.cerebro.run()
-        strategy = self.cerebro.strategies[0]
-        signal = strategy.generate_bbi_signal()
-        self.assertIn(signal, [-1, 0, 1])
-
-    def test_generate_pm_signal(self):
-        self.cerebro.run()
-        strategy = self.cerebro.strategies[0]
-        signal = strategy.generate_pm_signal()
-        self.assertIn(signal, [-1, 0, 1])
-
-    def test_generate_gp_signal(self):
-        self.cerebro.run()
-        strategy = self.cerebro.strategies[0]
-        signal = strategy.generate_gp_signal()
-        self.assertIn(signal, [-1, 0, 1])
-
-    # def test_execute_trade(self):
-    #     self.cerebro.run()
-    #     strategy = self.cerebro.strategies[0]
-    #     initial_cash = strategy.broker.get_cash()
-    #     strategy.final_signal = 1
-    #     strategy.execute_trade()
-    #     self.assertNotEqual(strategy.broker.get_cash(), initial_cash)'''
 ################# 生成BBI指标
 # 计算BBI指标
 def calculate_bbi(df):
@@ -120,7 +71,7 @@ def golden_pit_strategy(df, ma_period=60, drop_threshold=0.15):
     
     # 综合信号
     df['signal'] = 0
-    df.loc[pit_condition & recovery_condition, 'signal'] = 1  # 延迟2日确认
+    df.loc[pit_condition.shift(8) & recovery_condition, 'signal'] = 1  # 延迟2日确认
     # df.loc[pit_condition, 'signal'] = 1  
     # df.loc[recovery_condition, 'signal'] = 1
     df.loc[df['close'] < 0.97 * df['close'].rolling(10).min(), 'signal'] = -1
@@ -128,9 +79,13 @@ def golden_pit_strategy(df, ma_period=60, drop_threshold=0.15):
 
 
 #################自定义策略
-def pit_up_strategy(df, ma_period=60, drop_threshold=0.15):
+def pit_up_strategy(df, ma_period=30, drop_threshold=0.15):
     # 计算技术指标
-    df['MA60'] = df['close'].rolling(ma_period).mean()
+    df['MA30'] = df['close'].rolling(ma_period).mean()
+    df['MA20'] = df['close'].rolling(20).mean()
+    df['MA10'] = df['close'].rolling(10).mean()
+    df['MA5'] = df['close'].rolling(5).mean()
+
     df['Vol_MA5'] = df['volume'].rolling(5).mean()
     
     # 检测缩量挖坑
@@ -143,13 +98,17 @@ def pit_up_strategy(df, ma_period=60, drop_threshold=0.15):
     # 计算上影线和下影线长度
     df['upper_shadow'] = df['high'] - df[['open', 'close']].max(axis=1)  # 上影线 = 最高价 - max(开盘价, 收盘价)
     df['lower_shadow'] = df[['open', 'close']].min(axis=1) - df['low']   # 下影线 = min(开盘价, 收盘价) - 最低价
+    # 当日实体阴线或阳线长度
+    df['body'] = abs(df['open'] - df['close'])
     
     # 检测新的买入信号条件
     buy_condition = (
         (df['close'] < df['open']) &  # 当日收阴
         (df['close'].shift(1) > df['open'].shift(1)) &  # 前日收阳
         (df['volume'] < df['volume'].shift(1)) &  # 较比上一日缩量
-        (df['upper_shadow'] > df['lower_shadow'])  # 当日上影线长于下影线
+        (df['upper_shadow'] > df['lower_shadow']) &  # 当日上影线长于下影线
+        (df['body'] < df['upper_shadow']) & # 当日实体部分小于上影线
+        ~((df['close'] > df['MA30']) | (df['close'] > df['MA20']) | (df['close'] > df['MA10']) | (df['close'] > df['MA5']))   # 收盘价在MA30之上
     )
     
     # 综合信号
@@ -159,9 +118,54 @@ def pit_up_strategy(df, ma_period=60, drop_threshold=0.15):
     
     return df
 
+def fetch_data_from_db(symbol, conn):
+    """从数据库中读取数据"""
+    query = f"SELECT * FROM {symbol}"
+    try:
+        df = pd.read_sql(query, conn, index_col='date', parse_dates=['date'])
+        return df
+    except Exception as e:
+        print(f"Error reading data for {symbol}: {e}")
+        return pd.DataFrame()
+
+def main():
+    # 连接到数据库
+    conn = sqlite3.connect('./data/stock_data.db')
+    
+    # 获取所有股票代码
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    
+    # 遍历所有表（股票代码）
+    for table in tables:
+        symbol = table[0]  # 表名即股票代码
+        # print(f"Processing {symbol}")
+        
+        try:
+            # 从数据库中读取数据
+            df = fetch_data_from_db(symbol, conn)
+            if df.empty:
+                continue
+            
+            # 应用策略
+            # df = pit_up_strategy(df)
+            # if df['signal'].iloc[-1] == 1:
+            #     print(f"pit_pu: {symbol}: {df[['close', 'MA10', 'signal']].tail(1)}")
+            
+            df = golden_pit_strategy(df)
+            if df['signal'].iloc[-1] == 1:
+                print(f"golden_pit: {symbol}: {df[['close', 'MA60', 'signal']].tail(1)}")
+        
+        except Exception as e:
+            print(f"Error processing {symbol}: {e}")
+    
+    # 关闭数据库连接
+    conn.close()
 
 
 if __name__ == '__main__':
+    main()
     
     '''# BBI示例调用
     df = calculate_bbi(df)
@@ -172,19 +176,19 @@ if __name__ == '__main__':
     # df = golden_pit_strategy(df)
     # print(df[['close', 'MA60', 'signal']].tail())
     # 遍历data文件夹下的所有股票数据
-    for filename in os.listdir('./data'):
-        print(filename)
-        try:
-            df = pd.read_csv('./data/' + filename, index_col='date', parse_dates=True)
-        except Exception as e:
-            print(f"Error reading {filename}: {e}")
-            continue
-        df = pit_up_strategy(df)
-        if df['signal'].iloc[-1] == 1:
-            print(f"pit_pu: {filename}: {df[['close', 'MA60', 'signal']].tail(1)}")
-        df = golden_pit_strategy(df)
-        if df['signal'].iloc[-1] == 1:
-            print(f"golden_pit: {filename}: {df[['close', 'MA60', 'signal']].tail(1)}")
+    # for filename in os.listdir('./data'):
+    #     print(filename)
+    #     try:
+    #         df = pd.read_csv('./data/' + filename, index_col='date', parse_dates=True)
+    #     except Exception as e:
+    #         print(f"Error reading {filename}: {e}")
+    #         continue
+    #     df = pit_up_strategy(df)
+    #     if df['signal'].iloc[-1] == 1:
+    #         print(f"pit_pu: {filename}: {df[['close', 'MA60', 'signal']].tail(1)}")
+    #     df = golden_pit_strategy(df)
+    #     if df['signal'].iloc[-1] == 1:
+    #         print(f"golden_pit: {filename}: {df[['close', 'MA60', 'signal']].tail(1)}")
     # df = pd.read_csv('./data/sh600589.csv', index_col='date', parse_dates=True)
     # # 打印末尾几行数据
     # print(df.tail())
